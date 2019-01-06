e19f205a19eadac61946abc95fb04613a4b4d8a7
Manoj Selvin <manoj.selvin@yocket.in>
2019-01-06 23:39:25 +0530

    Included Request Logging Functionality

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
+// Built-in Modules
 const express = require('express');
+const fs = require('fs');
+
+// Third-party Modules
 const hbs = require('hbs');
 
+// Creating Express App
 var app = express();
 
-
+// Setting Partials Directory
 hbs.registerPartials(`${__dirname}/views/partials`)
+
+// Registering Helpers
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
+
+// Setting template engine
 app.set('view engine', 'hbs');
+
+// Logging Requests to file
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    fs.appendFile('server.log', `${log}\n`);
+    next();
+})
+
+// Static Files Middleware
 app.use(express.static(`${__dirname}/public`));
 
 var port = 3000;
